57ea64178e95f4119bf07c88668a1765306fda12
shuvo-220 <shuvo.info220@gmail.com>
2025-10-11 16:23:22 +0600

    frontend domain

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.get('', (req,res)=>{
 
 app.use(express.json());
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://tangerine-taiyaki-bb69b4.netlify.app',
     credentials:true
 }))
 
